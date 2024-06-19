afcf6bd834c0e6710adf2de8a182e5cea282a60a
Markislost <140057175+Markislost@users.noreply.github.com>
2024-06-19 10:20:20 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const panels = document.querySelectorAll('.panel')
+const showBtn = document.querySelector('.navBtn');
+const topNav = document.querySelector('.top-nav');
+
+showBtn.addEventListener('click', function(){
+    if(topNav.classList.contains('showNav')){
+        topNav.classList.remove('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
+    } else {
+        topNav.classList.add('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-times"></i>';
+    }
+});
 
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
-
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
-}
+/*lightbox*/
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
